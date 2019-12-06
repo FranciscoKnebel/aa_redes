780f0799157826a52ef8446a33e7aad2f564591b
Francisco Knebel <franciscopaivaknebel@gmail.com>
2019-12-05 23:20:50 -0200

    Update

--- client.py
+++ client.py
@@ -7,64 +7,59 @@ def main():
     port = ''
 
     try:
-      opts, args = getopt.getopt(sys.argv[1:], "hop:v", ["help", "output=", "port="])
-
-      if len(args) >= 1:
-        host = args[0]
-      else:
-        print('client.py <host>. Host is needed.')
-        sys.exit(3)
+      opts, args = getopt.getopt(sys.argv[1:], 'hp:v', ['help', 'port='])
     except getopt.GetoptError as err:
       print('client.py <host> -p <port>')
       print(err)
       sys.exit(2)
 
-    print(args)
     for opt, arg in opts:
-      print('opt ', opt)
-
       if opt in ['-h', '--help']:
         print('client.py <host> -p <port>')
         sys.exit(4)
       elif opt in ("-p", "--port"):
         port = arg
 
+    if len(args) >= 1:
+      host = args[0]
+    else:
+      print('client.py <host>. Host is needed.')
+      sys.exit(3)
+
     if (port == ''):
       port = 8080
 
-    # start_server(host, port)
+    connect_to_server(host, port)
 
 
-def start_server(HOST, PORT):
+def connect_to_server(HOST, PORT):
   print('Host: ', HOST)
   print('Port: ', PORT)
 
+  s = None
+  for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+      s = socket.socket(af, socktype, proto)
+    except OSError as msg:
+      s = None
+      continue
+    try:
+      s.connect(sa)
+    except OSError as msg:
+      s.close()
+      s = None
+      continue
+    break
+
+  if s is None:
+    print('could not open socket')
+    sys.exit(1)
+    
+  with s:
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+  print('Received', repr(data))
+
 main()
-
-# import socket
-# import sys
-
-# HOST = 'daring.cwi.nl'    # The remote host
-# PORT = 50007              # The same port as used by the server
-# s = None
-# for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
-#     af, socktype, proto, canonname, sa = res
-#     try:
-#         s = socket.socket(af, socktype, proto)
-#     except OSError as msg:
-#         s = None
-#         continue
-#     try:
-#         s.connect(sa)
-#     except OSError as msg:
-#         s.close()
-#         s = None
-#         continue
-#     break
-# if s is None:
-#     print('could not open socket')
-#     sys.exit(1)
-# with s:
-#     s.sendall(b'Hello, world')
-#     data = s.recv(1024)
-# print('Received', repr(data))
--- server.py
+++ server.py
@@ -5,67 +5,84 @@ import sys
 def main():
     port = ''
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"p:v",["port="])
+      opts, args = getopt.getopt(sys.argv[1:],'hp:v',['help', 'port='])
     except getopt.GetoptError as err:
-        print('server.py -p <port>')
-        print(err)
-        sys.exit(2)
+      print('server.py -p <port>')
+      print(err)
+      sys.exit(2)
         
     for opt, arg in opts:
-        if opt == '-h':
-            print('server.py -p <port>')
-            sys.exit()
-        elif opt in ("-p", "--port"):
-            port = arg
-
+      if opt in ('-h', '--help'):
+        print('server.py -p <port>')
+        sys.exit()
+      elif opt in ('-p', '--port'):
+        port = arg
 
     if (port == ''):
-        port = 8080
+      port = 8080
 
-    print('Defined Port: ', port)
+    print('Defined Port:', port)
   
     start_server(port)
 
 
+def get_host():
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  try:
+    # doesn't even have to be reachable
+    s.connect(('10.255.255.255', 1))
+    IP = s.getsockname()[0]
+  except:
+    IP = '127.0.0.1'
+  finally:
+    s.close()
+  return IP
+
 def start_server(PORT):
-    print('Starting server...')
+  HOSTNAME = get_host()
+  CONNECTION_LOCATION = HOSTNAME + ':' + PORT
+
+  print('Starting server on port', PORT)
+  print('Host', HOSTNAME)
+  print('')
+
+  HOST = None # Symbolic name meaning all available interfaces
+  s = None    # socket
   
-    HOST = None # Symbolic name meaning all available interfaces
-    s = None    # socket
+  for res in socket.getaddrinfo(
+    HOST, PORT, socket.AF_UNSPEC,
+    socket.SOCK_STREAM, 0, socket.AI_PASSIVE
+  ):
+    af, socktype, proto, canonname, sa = res
+    try:
+      s = socket.socket(af, socktype, proto)
+    except OSError as msg:
+      s = None
+      continue
+    try:
+      s.bind(sa)
+      s.listen(1)
+    except OSError as msg:
+      s.close()
+      s = None
+      continue
+    break
+
+  if s is None:
+    print('Could not open socket.')
+    sys.exit(1)
+
+  print('Server ready for connections on', CONNECTION_LOCATION)
+
+  conn, addr = s.accept()
+  with conn:
+    print('Connected by', addr)
+
+    while True:
+      data = conn.recv(1024)
     
-    for res in socket.getaddrinfo(
-      HOST, PORT, socket.AF_UNSPEC,
-      socket.SOCK_STREAM, 0, socket.AI_PASSIVE
-    ):
-      af, socktype, proto, canonname, sa = res
-      try:
-        s = socket.socket(af, socktype, proto)
-      except OSError as msg:
-        s = None
-        continue
-      try:
-        s.bind(sa)
-        s.listen(1)
-      except OSError as msg:
-        s.close()
-        s = None
-        continue
-      break
-
-    if s is None:
-      print('Could not open socket.')
-      sys.exit(1)
-
-    print('Server ready for connections.')
-
-    conn, addr = s.accept()
-    with conn:
-      print('Connected by', addr)
-
-      while True:
-        data = conn.recv(1024)
-     
-        if not data: break
-          conn.send(data)
+      if not data:
+        break
+      conn.send(data)
 
 main()
